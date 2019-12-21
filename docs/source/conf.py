c8c459ef9dc46e9b2ba8eaa878ea9860b74de217
Hugo Slepicka <slepicka@slac.stanford.edu>
2019-12-20 16:54:07 -0800

    WIP: Use .readthedocs.yml.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,11 @@ import sphinx_rtd_theme
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
+module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../')
+sys.path.insert(0,module_path)
+sys.path.insert(0,os.path.join(module_path,'pydm/widgets'))
+
+import pydm
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -36,14 +41,6 @@ extensions = [
     'sphinx.ext.githubpages',
     'sphinxcontrib.napoleon',
 ]
-
-autodoc_mock_imports = ["qtpy", "PyQt5"]
-
-module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../')
-sys.path.insert(0,module_path)
-sys.path.insert(0,os.path.join(module_path,'pydm/widgets'))
-
-import pydm
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
